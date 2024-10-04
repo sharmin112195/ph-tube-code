0802485ce4dac4e8749f513211dfc17102d8629e
shopna's Macbook <sharminshopna149@gmail.com>
2024-10-05 03:32:16 +0600

    ph-tube done

--- scripts/video.js
+++ scripts/video.js
@@ -23,6 +23,7 @@ const displayCategories = (categories) =>{
 }
 
 
+
 // click button function
 const lodeCategoryVideos = (id) =>{
 fetch(`https://openapi.programming-hero.com/api/phero-tube/category/${id}`)
@@ -50,8 +51,8 @@ for(let btn of buttons){
 
 
 // video section
-const loadVideo = () =>{
-  fetch(" https://openapi.programming-hero.com/api/phero-tube/videos")
+const loadVideo = (searchText ="") =>{
+  fetch(` https://openapi.programming-hero.com/api/phero-tube/videos? title = ${searchText}`)
   .then((res) => res.json())
   .then((data) => displayVideo(data.videos))
   .catch((error) => console.log(error))
@@ -87,28 +88,51 @@ const displayVideo = (videos) =>{
   </figure>
   <div class="">
      <div class="px-0 py-2 flex gap-2">
-     <img class="w-10 h-10 rounded-full object-cover" src=${video.authors[0].profile_picture}
+          <img class="w-10 h-10 rounded-full object-cover" src=${video.authors[0].profile_picture}
        </div>
-       <div>
-     <h2 class="fond-bold">${video.title}</h2>
-     <div class="flex items-center gap-2">
-     <p class="text-gray-400">${video.authors[0].profile_name}</p>
-     ${
-      video.authors[0].verified == true?`<img class="w-5" src="https://img.icons8.com/?size=96&id=D9RtvkuOe31p&format=png" />`:""      
-     }
-     ${
-      video.others.posted_date?.length == 0 ? "":`<span class="absolute right-3 bottom-20 bg-black rounded p-1 text-white text-xs">${getTimeString(video.others.posted_date)}</span>`
-     }
+         <div>
+           <h2 class="fond-bold">${video.title}</h2>
+              <div class="flex items-center gap-2">
+                 <p class="text-gray-400">${video.authors[0].profile_name}</p>
+                   ${
+                      video.authors[0].verified == true?`<img class="w-5" src="https://img.icons8.com/?size=96&id=D9RtvkuOe31p&format=png" />`:""      
+                    }
+                     ${
+                        video.others.posted_date?.length == 0 ? "":`<span class="absolute right-3 bottom-24 bg-black rounded p-1 text-white text-xs">${getTimeString(video.others.posted_date)}</span>`
+                      }
+                  
+       </div> 
 
-     
-       </div>
-    
+            <p><button onclick="loadDetails('${video.video_id}')" class="btn btn-sm btn-accent">details</button></p>
+
     </div>
   </div>
     `;
     videoContainer.append(card);
   })
 }
+
+// video id section er function
+ const loadDetails=async(videoId) =>{
+  console.log(videoId);
+  const url=`https://openapi.programming-hero.com/api/phero-tube/video/${videoId}`;
+  const res=await fetch(url);
+  const data=await res.json();
+  displayDetails(data.video);
+ }
+
+ const displayDetails=(video) =>{
+  console.log(video);
+  const detailContainer=document.getElementById("modal-content");
+  detailContainer.innerHTML=`
+  <img src=${video.thumbnail} />
+  <p>${video.description}</p>
+  `
+  // way-1
+  // document.getElementById("showModalData").click();
+  // way-2
+  document.getElementById("customModal").showModal();
+ }
 
 // time function
 
@@ -121,6 +145,11 @@ function getTimeString(time){
   return`${hour} hour ${minute} minute ${remainingSecond} second ago`;
 }
 
+// search value
+
+document.getElementById("search-input").addEventListener("keyup",(e)=>{
+  loadVideo(e.target.value);
+})
 
 
 loadCatagories()
